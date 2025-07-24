545a0e47da3e703577ac673a309a90332d98a968
Joshy <prism.executor@gmail.com>
2025-07-23 18:18:39 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,2 @@
-local uiInit = "https://raw.githubusercontent.com/joshy-5/Milk-script/refs/heads/main/ui-init.lua?token=GHSAT0AAAAAADG7AGGOELXT3X235UVBXI5A2EBQRVQ"
+local uiInit = "https://raw.githubusercontent.com/joshy-5/Milk-script/main/ui-init.lua"
+
